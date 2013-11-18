cfe8472592db0695e7d57e69c735830957b70508
segfault <s9gf4ult@gmail.com>
2013-11-18 23:30:51 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metatea::Application.config.secret_token = 'b0b1c0a46debeb38f56d1b487642fc44ff0c32299122fca51169f754ea5aea2ed1328e59c50246c1c3e78f38f8a20c8c030e756e73a27355f836a1002c484ace'
