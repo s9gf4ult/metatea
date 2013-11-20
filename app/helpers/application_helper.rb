de9dfc19bc639cd3e1d1ccfff67516a83ce168db
segfault <s9gf4ult@gmail.com>
2013-11-21 00:41:37 +0600

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  # convert hash to optarg tree
+  def opt_hash(h, selected)
+    h.collect do |k, v|
+      if v.is_a? Hash
+        content_tag :optgroup, :label => t(k) do
+          opt_hash(v, selected)
+        end
+      else
+        s = v == selected if selected
+        content_tag :option, :value => v, :selected => s do
+          t k
+        end
+      end
+    end.reduce(:concat)
+
+  end
+
+
 end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,6 @@
 class Tea < ActiveRecord::Base
   attr_accessible :category, :factory, :name
+
+  validates :name, :presence => true
+  validates :category, :presence => true
 end
