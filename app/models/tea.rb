19c632243508f3a99d3c9bc7c8598e9ef3ced658
segfault <s9gf4ult@gmail.com>
2013-11-23 18:05:24 +0600

    new file:   app/assets/stylesheets/blueprint/screen.css 	modified:   app/models/tea.rb 	new file:   app/views/layouts/_categories.html.haml 	new file:   app/views/layouts/_header.html.haml 	modified:   app/views/layouts/application.html.haml 	modified:   app/views/teas/index.html.haml

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -4,6 +4,6 @@ class Tea < ActiveRecord::Base
   validates :name, :presence => true
   validates :category, :presence => true
 
-  has_many :tea_pictures
-  has_many :tea_links
+  has_many :tea_pictures, :dependent => :destroy
+  has_many :tea_links, :dependent => :destroy
 end
