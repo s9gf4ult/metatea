290b7fafdff18c17937fdd3043266788f1eae9e3
segfault <s9gf4ult@gmail.com>
2013-12-02 12:17:54 +0600

    new file:   spec/requests/tea_spec.rb 	modified:   spec/spec_helper.rb

---
+++ spec/requests/tea_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'tea' do
+  before :each do
+    @user = FactoryGirl.create :user
+
+  end
+
+  it 'can be created' do
+    visit '/teas/new'
+    page.should_be valid
+  end
+end
